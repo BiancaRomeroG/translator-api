b33243b03c93ec4f54358c8a61357d24166f0d05
BiancaRomeroG <biancaromeroguz@gmail.com>
2022-12-30 22:49:35 -0400

    Change tokens

--- Shared/Constants.cs
+++ Shared/Constants.cs
@@ -7,11 +7,11 @@
 
         // Source
         public const string SOURCE_CONTAINERNAME = "source-container";
-        public const string SOURCE_CONTAINERTOKEN = "sp=rl&st=2022-09-15T18:06:21Z&se=2022-12-22T02:06:21Z&sv=2021-06-08&sr=c&sig=VRAbIDw3Sma4TTqjj3xAaTb4heXQmreI8LHoWJbCuKs%3D";
+        public const string SOURCE_CONTAINERTOKEN = "sp=racwdli&st=2022-12-31T02:39:06Z&se=2023-11-27T10:39:06Z&spr=https&sv=2021-06-08&sr=c&sig=Q6oaSYCC%2FD1n86pq44%2FbdHxEcewQ4Hm6ay5zvZLKXHs%3D";
 
         // Target
         public const string TARGET_CONTAINERNAME = "target-container";
-        public const string TARGET_CONTAINERTOKEN = "sp=wl&st=2022-09-15T18:08:06Z&se=2022-12-22T02:08:06Z&sv=2021-06-08&sr=c&sig=JXwNWJPVtmFdWhxTwu1or5jNv%2FXC%2BAcIRsajRmUnzuo%3D";
+        public const string TARGET_CONTAINERTOKEN = "sp=wl&st=2022-12-31T02:47:57Z&se=2022-12-31T10:47:57Z&spr=https&sv=2021-06-08&sr=c&sig=sIpeP0G%2F9m8OxbRXINLIflk2zPEhTF%2Foj9Tg7LokZTk%3D";
 
         // AITranslator
         public const string AITRANSLATOR_ENDPOINT = "https://ai-translator-service.cognitiveservices.azure.com/translator/text/batch/v1.0/batches";
